522c3c4de0081f3b8007de4a6c569196840b070a
Jon Price <jonprice77@hotmail.com>
2019-11-08 22:08:23 +1100

    Updates

---
+++ precache-manifest.6b62ff8f1cc604f2871f849fa8623e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e408896b64ef96471feaab75493c1b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "412b7931e9a60916149f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0485233f5eb25ddaedc2",
+    "url": "/static/js/2.b6a4e068.chunk.js"
+  },
+  {
+    "revision": "412b7931e9a60916149f",
+    "url": "/static/js/main.587e213f.chunk.js"
+  },
+  {
+    "revision": "a574de28d16a5a9ee2ab",
+    "url": "/static/js/runtime-main.5fe9cbad.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
